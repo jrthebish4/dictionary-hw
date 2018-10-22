aec0e57e66a44222946a1dd3775e848d6a61465d
Jake <jakobrodney94@gmail.com>
2018-10-22 07:22:43 -0700

    Word break complete with tests

--- src/test/java/DictionaryTest.java
+++ src/test/java/DictionaryTest.java
@@ -1,28 +1,25 @@
 import org.junit.Test;
+import util.Dictionary;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by Jake on 10/17/18.
+ * Tests the Dictionary class
+ * Created by Jake on 10/19/18.
  */
 public class DictionaryTest {
 
     @Test
     public void isEnglishWord() throws Exception {
         Dictionary dictionary = new Dictionary();
-        assertTrue(dictionary.isEnglishWord("snake"));
+        assertTrue(dictionary.isEnglishWord("run"));
+        assertTrue(dictionary.isEnglishWord("climb"));
+        assertTrue(dictionary.isEnglishWord("jump"));
+
+        assertFalse(dictionary.isEnglishWord("notaword"));
+        assertFalse(dictionary.isEnglishWord("alsonotaword"));
+        assertFalse(dictionary.isEnglishWord("definitelynotaword"));
     }
-
-//    public static void main(String args[]) {
-//        org.junit.runner.JUnitCore.main("DictionaryTest");
-//    }
-
-    @Test
-    public void findWordsInAString() throws Exception{
-        Dictionary dictionary = new Dictionary();
-        StringPermutations stringPermutations = new StringPermutations(dictionary);
-        stringPermutations.findWordsInString("12345");
-    }
-
 
 }
